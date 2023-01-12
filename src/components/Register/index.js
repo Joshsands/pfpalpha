a12ed175d0eb6052358ebf7b9993f4bd70c05e92
Joshua Sands <83298011+Joshsands@users.noreply.github.com>
2023-01-11 20:17:44 -0600

    started auth

---
+++ src/components/Register/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Form, Button, Card } from 'react-bootstrap'
+
+export default function Signup () {
+    return (
+        <>
+        <Card>
+            <Card.Body>
+                <h2 className="text-center mb-4">Sign Up</h2>
+            </Card.Body>
+        </Card>
+        </>
+    )
+}
