a60f795b440a33f02b3abfba95eca19d69ad8436
Joshua Sands <83298011+Joshsands@users.noreply.github.com>
2023-01-14 12:27:06 -0600

    started protfolio

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,10 +1,90 @@
 import * as React from "react";
+import { Card, Container } from 'react-bootstrap'
 
 const Portfolio = () => {
   return (
-    <div>
-      THIS IS THE CONTACT PAGE
-    </div>
+    <>
+  
+
+    <Container className="d-flex align-items-center justify-content-center">
+      <Card className='w-50 align-center'>
+
+        <Card.Body>
+          <h2 className="text-center mb-4">OUR PROJECT HISTORY</h2>
+</Card.Body>
+
+</Card>
+
+
+</Container>
+
+    <Container className="d-flex align-items-center justify-content-center">
+      <Card className='w-50 align-center'>
+
+        <Card.Body>
+          <h2 className="text-center mb-4">PROJECT 1</h2>
+          <div>
+          <img src={'project1'} alt="project 1" />
+          <p>
+            Tesla
+          </p>
+          </div>
+        
+        </Card.Body>
+ 
+      </Card>
+
+      <Card className='w-50 align-center'>
+
+<Card.Body>
+  <h2 className="text-center mb-4">PROJECT 1</h2>
+  <div>
+  <img src={'project1'} alt="project 1" />
+  <p>
+    Tesla
+  </p>
+  </div>
+
+</Card.Body>
+
+</Card>
+
+
+</Container>
+
+<Container className="d-flex align-items-center justify-content-center">
+      <Card className='w-50 align-center'>
+
+        <Card.Body>
+          <h2 className="text-center mb-4">PROJECT 1</h2>
+          <div>
+          <img src={'project1'} alt="project 1" />
+          <p>
+            Tesla
+          </p>
+          </div>
+        
+        </Card.Body>
+ 
+      </Card>
+
+      <Card className='w-50 align-center'>
+
+<Card.Body>
+  <h2 className="text-center mb-4">PROJECT 1</h2>
+  <div>
+  <img src={'project1'} alt="project 1" />
+  <p>
+    Tesla
+  </p>
+  </div>
+
+</Card.Body>
+
+</Card>
+
+</Container>
+</>
   );
 };
 
